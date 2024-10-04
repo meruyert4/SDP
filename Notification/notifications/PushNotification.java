439c2b0b68b1f1aa3572e604ceccca6270c6908f
meruyert4 <147834105+meruyert4@users.noreply.github.com>
2024-10-04 17:39:16 +0500

    Done Tas2 and added explanation for task 2

---
+++ Notification/notifications/PushNotification.java
@@ -0,0 +1,16 @@
+package notifications;
+
+import java.util.List;
+
+public class PushNotification implements Notification {
+    @Override
+    public void send(String recipient, String message) {
+        System.out.println("Sending Push Notification to " + recipient + ": " + message);
+    }
+
+    public void sendBulkNotification(List<String> recipients, String message) {
+        for (String recipient : recipients) {
+            send(recipient, message);
+        }
+    }
+}
---
+++ Notification/notifications/SMSNotification.java
@@ -0,0 +1,16 @@
+package notifications;
+
+import java.util.List;
+
+public class SMSNotification implements Notification {
+    @Override
+    public void send(String recipient, String message) {
+        System.out.println("Sending SMS to " + recipient + ": " + message);
+    }
+
+    public void sendBulkNotification(List<String> recipients, String message) {
+        for (String recipient : recipients) {
+            send(recipient, message);
+        }
+    }
+}
